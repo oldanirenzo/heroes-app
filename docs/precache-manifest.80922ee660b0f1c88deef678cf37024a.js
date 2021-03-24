9cbd565821fd7e9e4c31c371a1bd3cb580ede977
Renzo Oldani <oldanirenzo@gmail.com>
2021-03-24 18:40:06 -0300

    docs agregado

---
+++ docs/precache-manifest.80922ee660b0f1c88deef678cf37024a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21650853d17bcef70158d00a100f854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae72b58cef15eba76459",
+    "url": "/static/js/2.08d328db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08d328db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6bf9c136cd8395bd8c",
+    "url": "/static/js/main.f25e8d3f.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
